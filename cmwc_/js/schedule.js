71df78a831e1b3ce4c881816d54d2067f4942cea
Deco Worcman <85200242+DecoArtist@users.noreply.github.com>
2023-02-10 21:56:48 -0300

    Delete cmwc_ directory

--- cmwc_/js/schedule.js
+++
@@ -1,8 +0,0 @@
-export default () => /*html*/`
-    <div class="container p-relative">
-        <div class="d-flex justify-content-center">
-            <h1>Schedule</h1>
-            <p>CMWC schedule page here...</p>
-        </div>
-    </div>
-`;
